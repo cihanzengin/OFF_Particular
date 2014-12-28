931be0ee8cc4616c64bf1a172e1b6595790e471f
Cihan Zengin <ckirkov@gmail.com>
2014-12-28 19:24:23 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,11 @@
-#include "ofMain.h"
 #include "ofApp.h"
+#include "ofAppGlutWindow.h"
 
-//========================================================================
-int main( ){
-	ofSetupOpenGL(1024,768,OF_WINDOW);			// <-------- setup the GL context
-
-	// this kicks off the running of my app
-	// can be OF_WINDOW or OF_FULLSCREEN
-	// pass in width and height too:
-	ofRunApp(new ofApp());
-
+//--------------------------------------------------------------
+int main(){
+    ofAppGlutWindow window; // создать окно
+    // набор ширина, высота, режим (OF_WINDOW or OF_FULLSCREEN)
+    ofSetupOpenGL(&window, 1024, 768, OF_WINDOW);
+    ofRunApp(new testApp()); // запустить приложение
+  
 }
